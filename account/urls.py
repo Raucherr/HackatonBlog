cb18869975ebc8d01376536927190a3180854928
TimJo94 <timurjorobekov@gmail.com>
2022-04-13 12:26:44 +0600

    sc

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import *
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='registration'),
@@ -11,5 +11,6 @@ urlpatterns = [
     path('profile-update/<int:pk>/', ProfileUpdateView.as_view()),
     path('forgot_pass/', ForgotPasswordView.as_view(), name="forgot-password"),
     path('change_password/', ChangePasswordView.as_view(), name="change-password"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
