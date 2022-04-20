7966c77ce331bd917beeb2f6b58d137b073e0a4f
TimJo94 <timurjorobekov@gmail.com>
2022-04-20 05:16:49 +0000

    from server

--- post/views.py
+++ post/views.py
@@ -7,16 +7,32 @@ from django_filters import rest_framework as filters
 from post.models import Post
 from post.permissions import IsPostAuthor
 from post.serializer import PostSerializer
+from django.db.models import Q
+from rest_framework.decorators import action
+#import django_filters
 
+#class PostFilter(django_filters.FilterSet):
+#    title = django_filters.CharFilter(lookup_expr='iexact')
+#    description = django_filters.CharFilter(lookup_expr='iexact')
+
+#    class Meta:
+#        model = Post
+#        fields = ['title', 'description']
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     filter_backends = [SearchFilter, OrderingFilter, filters.DjangoFilterBackend]
-    search_fields = ['title', 'author', 'category']
-    ordering_fields = ['category', 'title']
+    search_fields = ('title', 'description')
 
-    # pagination_class = AdPagination
+    @action(detail=False, methods=['get'])              #action decorator is available ONLY with viewsets
+    def search(self, request, pk=None):                 #router builds path posts/search/?q=paris
+        # print(request.query_params)
+        q = request.query_params.get('q')               #<----=----->request.GET
+        queryset = self.get_queryset()
+        queryset = queryset.filter(Q(title__icontains=q) | Q(description__icontains=q))
+        serializer = PostSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def get_serializer_class(self):
         serializer_class = super().get_serializer_class()
@@ -30,3 +46,10 @@ class PostViewSet(ModelViewSet):
         elif self.action in ['update', 'partial_update', 'destroy']:
             return [IsPostAuthor()]
         return []
+
+    @action(detail=False, methods=['get'])
+    def my_posts(self, request, pk=None):
+        queryset = self.get_queryset()
+        queryset = queryset.filter(email=request.user)
+        serializer = PostSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
